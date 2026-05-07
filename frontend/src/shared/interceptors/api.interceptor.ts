36db8f20b22e6d0b05ff045d3593329dfaeb148d
IgnIVertiKalCaD <idrobot696@gmail.com>
2026-05-07 20:45:36 +0300

    Remove comments from api.interceptor.ts

--- frontend/src/shared/interceptors/api.interceptor.ts
+++ frontend/src/shared/interceptors/api.interceptor.ts
@@ -1,12 +1,10 @@
 import {HttpInterceptorFn} from "@angular/common/http";
 
 export const apiInterceptor: HttpInterceptorFn = (req, next) => {
-    // Если запрос уже содержит http:// или https://, не трогаем его
     if (req.url.startsWith('http')) {
         return next(req);
     }
 
-    // Убеждаемся, что относительный путь не начинается со слеша перед склейкой
     const path = req.url.startsWith('/') ? req.url.substring(1) : req.url;
     const url = `/api/${path}`;
 
