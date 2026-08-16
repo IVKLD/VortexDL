9a5e738f56fb54d0b0824e4219b0014c4f627021
IgnIVertiKalCaD <idrobot696@gmail.com>
2026-08-16 07:33:58 +0300

    Add youtube lib

--- libs/yt-audio-downloader/src/extractor/strategy.rs
+++
@@ -1,8 +0,0 @@
-use crate::{error::Result, models::ExtractedMedia};
-
-pub trait MediaExtractor: Send + Sync {
-    fn extract<'a>(
-        &'a self,
-        target: &'a str,
-    ) -> std::pin::Pin<Box<dyn std::future::Future<Output = Result<ExtractedMedia>> + Send + 'a>>;
-}
